9c0ab328623226c17ffff211345701f5789bba34
Juan Olpindo <dad3ztroyah@gmail.com>
2026-02-24 02:59:21 +0800

    Added profile implementation

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    email = models.EmailField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    
+    @property
+    def __str__(self):
+        return self.first_name + " " + self.last_name
