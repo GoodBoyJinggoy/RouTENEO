2f7241396acbc27dab0bacd3b0931524f50426ae
Juan Olpindo <dad3ztroyah@gmail.com>
2026-02-27 12:00:47 +0800

    Added create user view

--- project_routeneo/accounts/views.py
+++ project_routeneo/accounts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import AllowAny
 
-#https://realpython.com/django-user-management/
-#https://www.youtube.com/watch?v=tUqUdu0Sjyc
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
