8cb13227203aa0d3189de212d9271ff0a105bb88
Juan Olpindo <dad3ztroyah@gmail.com>
2026-02-27 12:10:41 +0800

    Added auth url

--- project_routeneo/accounts/urls.py
+++ project_routeneo/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path("user/register/", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("accounts-auth/", include("rest_framework.urls")),
 ]
