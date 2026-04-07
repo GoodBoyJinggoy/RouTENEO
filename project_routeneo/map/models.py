c38f155be0c1afe06eef9b950ec8186eb1b7f327
Juan Olpindo <dad3ztroyah@gmail.com>
2026-04-07 19:08:07 +0800

    Added route model

--- project_routeneo/map/models.py
+++ project_routeneo/map/models.py
@@ -4,3 +4,9 @@ class Building(models.Model):
     name = models.CharField(max_length=255)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+class Route(models.Model):
+    route_id = models.IntegerField()
+    route_name = models.CharField(max_length=255)
+    start_building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="route_start")
+    end_building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="route_end")
