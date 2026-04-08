f94415f124a30ef0b68eea42e9cc1fd58786133e
Juan Olpindo <dad3ztroyah@gmail.com>
2026-04-09 04:29:12 +0800

    Added route and comment serializers

--- project_routeneo/map/serializers.py
+++ project_routeneo/map/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Building
+from .models import Building, Route, Comment
 
 class BuildingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Building
-        fields = ["name", "latitude", "longitude"]
+        fields = ["building_id", x"name", "latitude", "longitude"]
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ["route_id", "route_name", "start_building", "end_building"]
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["comment_id", "date", "content", "user", "route"]
