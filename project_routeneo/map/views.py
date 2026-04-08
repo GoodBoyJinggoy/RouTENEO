18244c40174386b0d047a57e65c7b07ce99647fa
Juan Olpindo <dad3ztroyah@gmail.com>
2026-04-09 04:37:55 +0800

    Added comment view and comment add view

--- project_routeneo/map/views.py
+++ project_routeneo/map/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from .models import Building
-from .serializers import BuildingSerializer
+from .models import Building, Comment
+from .serializers import BuildingSerializer, CommentSerializer
+from rest_framework import generics, permissions
 
 # https://av.tib.eu/media/45441
 # https://www.geeksforgeeks.org/python/viewsets-routers-django-rest-framework/
@@ -8,3 +9,12 @@ from .serializers import BuildingSerializer
 class BuildingViewSet(ReadOnlyModelViewSet):
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
+
+class CommentView(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class CommentAddView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
