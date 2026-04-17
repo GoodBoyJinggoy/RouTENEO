314886b2ff8068798870980acdfbc9adc87d9740
Juan Olpindo <dad3ztroyah@gmail.com>
2026-04-17 19:22:06 +0800

    Created a route view

--- project_routeneo/map/views.py
+++ project_routeneo/map/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from .models import Building, Comment
-from .serializers import BuildingSerializer, CommentSerializer
+from .models import Building, Comment, Route
+from .serializers import BuildingSerializer, CommentSerializer, RouteSerializer
 from rest_framework import generics, permissions
 
 # https://av.tib.eu/media/45441
@@ -18,3 +18,7 @@ class CommentAddView(generics.CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class RouteView(ReadOnlyModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
